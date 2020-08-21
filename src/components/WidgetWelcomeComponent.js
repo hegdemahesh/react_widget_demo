b30f41efb18270a6b12d0df0274ac92a048f19ac
Maheshchandra Hegde <hid.mahesh@gmail.com>
2020-08-21 16:58:17 +0530

    changes #1016

--- src/components/WidgetWelcomeComponent.js
+++ src/components/WidgetWelcomeComponent.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import bg from '../assets/bgImage01.png';
+import './WidgetWelcomeComponent.css';
+
 
 export default class WidgetWelcomeComponent extends React.Component {
     constructor(props){
         super(props);
     }
     render() {
-        return <div>widget welcome</div>
+        let widgetElement = 
+        <div className='widgetWelcomeHolder'>
+            <div className='widgetBg'>
+                <img className="bgImage" src={bg} />
+            </div>
+            <div className="spacerv"></div>
+            <div className="welcomeWidgetContent">
+                <div className="contentItems">Logo here</div>
+                <div className="contentItems" >Welcome Text Herecv
+                    sdasdfadsfsadfas
+                    asfasfasdfasdfds
+                    asfsddfdfsfdasfdas
+                    xccvxz zzvxccvcvzvczcxxxxxxxxxxxxxxxxxxxxxxxx</div>
+            </div>
+        </div>;
+        return widgetElement;
     }
 }
