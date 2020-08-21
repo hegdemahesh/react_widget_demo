24b4b489bfd5244beecb06d33922aa76b190b641
Maheshchandra Hegde <hid.mahesh@gmail.com>
2020-08-21 13:08:45 +0530

    selectableCard added

---
+++ src/components/selectableCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './selectableCard.css';
+
+class SelectableCard extends React.Component {
+    render() {
+        let cardUIElement = 
+        <div className="cardElement">
+
+        </div>;
+        return (cardUIElement);
+    }
+}
+
+export default SelectableCard;
--- src/components/widgetHomeComponent.js
+++ src/components/widgetHomeComponent.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './widgetHomeComponent.css';
 import hlogo from '../assets/HLogo.png';
+import plusLogo from '../assets/plusLogo.png';
+import otheLogo from '../assets/otheLogo.png';
+import SelectableCard from './selectableCard';
+
 
 class WidgetHomeComponent extends React.Component {
     
@@ -10,8 +14,18 @@ class WidgetHomeComponent extends React.Component {
             <div className="LogoBox">
                 <img alt='hlogo'
                  src={hlogo} />
+                 <img alt='pluslogo'
+                 src={plusLogo} />
+                 <img alt='otheLogo'
+                 src={otheLogo} />
             </div>
-            <div>Content 2</div>
+            <div className="headerTextBox">
+                <h4>Connect with Intercom</h4>
+                <p>Choose the account you want to connect with</p>
+            </div>
+            <div>
+                <SelectableCard></SelectableCard>
+            </div>
         </div>;
         return divElement;
     }
